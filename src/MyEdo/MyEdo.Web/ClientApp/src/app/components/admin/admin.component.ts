de3b6e378a5da9f689838340c8e124ef946ba346
Dimo Yanev <Dimo_Yanev@abv.bg>
2021-08-26 23:50:39 +0300

    icons on lock/unlock user

--- src/MyEdo/MyEdo.Web/ClientApp/src/app/components/admin/admin.component.ts
+++ src/MyEdo/MyEdo.Web/ClientApp/src/app/components/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import UserModel from "src/app/core/models/user-model";
-import { AdminService } from "src/app/core/services/admin.service";
+import { Observable } from "rxjs";
+import UserModel from "../../core/models/user-model";
+import { AdminService } from "../../core/services/admin.service";
 
 @Component({
   selector: "app-admin",
@@ -8,20 +9,20 @@ import { AdminService } from "src/app/core/services/admin.service";
   styleUrls: ["./admin.component.css"],
 })
 export class AdminComponent implements OnInit {
-  public users: UserModel[];
+  users: UserModel[];
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit() {
-    this.adminService.getAllUsers().subscribe((data) => {
-      this.users = data;
-      console.log(this.users);
-    });
+    this.adminService.getAllUsers().subscribe((data) => (this.users = data));
   }
 
-  public onSaveChanged(userId: string, lockUser: boolean): void {
+  public async onSaveChanged(userId: string, lockUser: boolean): void {
     lockUser
-      ? this.adminService.lockUser(userId).subscribe((response) => {})
-      : this.adminService.unLockUser(userId).subscribe((response) => {});
+      ? this.adminService.unLockUser(userId).subscribe()
+      : this.adminService.lockUser(userId).subscribe();
+    this.users
+      .filter((u) => u.id == userId)
+      .map((p) => (p.isLocked = !lockUser));
   }
 }
