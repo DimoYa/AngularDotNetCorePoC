5602d7332750eeac15492af5037a3e4db8859c9c
Dimo Yanev <Dimo_Yanev@abv.bg>
2021-08-29 23:56:31 +0300

    rename controller name

--- src/MyEdo/MyEdo.Web/Controllers/AdminController.cs
+++ src/MyEdo/MyEdo.Web/Controllers/AdminController.cs
@@ -30,11 +30,11 @@ namespace MyEdo.Controllers
             this.userService = userService;
         }
 
-        [HttpGet]
+        [HttpGet(nameof(GetAllUsers))]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<IEnumerable<GetAllUsersApiModel>>> GetAllSkillsByCategories()
+        public async Task<ActionResult<IEnumerable<GetAllUsersApiModel>>> GetAllUsers()
         {
             try
             {
